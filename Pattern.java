07e049d51e1dadf82ca1568f0a1fc0ebe59f5604
NoorainBhure <noorainbhure123@gmail.com>
2026-02-02 12:33:17 +0530

    u

--- Pattern.java
+++ Pattern.java
@@ -21,16 +21,98 @@ public class Pattern {
         //     System.out.println();
         // }
 
-        int n3=5;
-        for(int i=n3;i>=n3;i++){
-            
+       
+        System.out.println();
+
+          System.out.println("----Solid Rectangle----");
+          for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+          }
+          System.out.println("-------Hollow rectangle----");
+          int n3=4;
+          for(int i=1;i<=n3;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1||j==1||i==n3||j==5){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+          System.out.println();  
+          }
+          System.out.println("------HAlf pyramid-------");
+          int n4=4;
+          for(int i=1;i<=n4;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
-        }
-        System.out.println();
+            System.out.println();
+          }
 
           
+          System.out.println("--inverted pyramid---");
+          int n5=5;
+          for(int i=n5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+          }
+
+          System.out.println("=------Other incerted pyramid----");
+          for(int i=1;i<=n5;i++){
+            for(int j=1;j<=n5-i;j++){
+                System.out.print(" ");
+            }for(int k=1;k<=i;k++){
+            System.out.print("*");
+          }
+          System.out.println();
+          }
+
+
+
+
+           System.out.println("=------Other incerted pyramid----");
+          for(int i=n5;i>=1;i--){
+            for(int j=1;j<=n5-i;j++){
+                System.out.print(" ");
+            }for(int k=1;k<=i;k++){
+            System.out.print("*");
+          }
+          System.out.println();
+          }
+
+
+          System.out.println("-------Floyds triangle----");
+          int num=1;
+          for(int i=1;i<=n5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num++;
+            }
+            System.out.println();
+
+          }
+          System.out.println("------0 1 triangle---");
+          for(int i=1;i<=n5;i++){
+            for(int j=1;j<=i;j++){
+                 int sum=i+j;
+                if(sum %2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }System.out.println();
+          }
+          for(int i=1;i<=n5;i++){
+            for(int j=1;j<=n5-i+1;j++){
+                System.out.print(j);
+
+            }System.out.println();
+          }
     }
     
 }
