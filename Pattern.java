d02d9e28010b674be7798919bf4a04f46836590e
NoorainBhure <noorainbhure123@gmail.com>
2026-02-03 21:09:40 +0530

    update

--- Pattern.java
+++ Pattern.java
@@ -113,6 +113,71 @@ public class Pattern {
 
             }System.out.println();
           }
+          System.out.println("=------Other incerted pyramid----");
+          int n=5;
+          for(int i =1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+              System.out.print("*");
+            }
+            System.out.println();
+          }
+          
+ System.out.println("=------Pyramid----");
+ n=5;
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=n-i;j++){
+    System.out.print(" ");
+  } 
+ for(int k=1;k<=i;k++){
+  System.out.print(" *");
+ }System.out.println();
+ }
+ System.out.println("=------Inverted Pyramid----");
+ n=5;
+ for(int i=n;i>=1;i--){
+  for(int j=1;j<=n-i;j++){
+    System.out.print(" ");
+    
+    }for(int k=1;k<=i;k++){
+      System.out.print(" * ");
+  } 
+
+ System.out.println();
+ }
+
+
+ System.out.println("=------Rhombus----");
+
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=n-i;j++){
+    System.out.print(" ");}
+  int j=1;
+  for(int k=1;k<=n;k++){
+     System.out.print(" *");
+  }
+  System.out.println();
+  } 
+  
+ System.out.println("=------Hollow Pyramid----");
+
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=n-i;j++){
+    System.out.print(" ");}
+  int j=1;
+  for(int k=1;k<=i;k++){
+    if(i==1||i==n){
+     System.out.print(" *");}
+     else{
+      for(int l=1;l<=n-j;l++){
+        System.out.print("u");
+      }
+     }
+  }
+  System.out.println();
+  } 
+ }
+ 
+
     }
     
-}
+
